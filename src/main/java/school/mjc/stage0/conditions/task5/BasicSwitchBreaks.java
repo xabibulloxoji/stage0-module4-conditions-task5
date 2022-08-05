9526ad140376537dde622c77ef14891ee450eb8e
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-05 11:45:45 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
@@ -4,14 +4,10 @@ public class BasicSwitchBreaks {
     public static void main(String[] args) {
         int a = 20;
         switch (a) {
-            default:
-                System.out.println("default");
-            case 7:
-                System.out.println("seven");
-            case 10:
-                System.out.println("ten");
-            case 20:
-                System.out.println("twenty");
+            case 7 -> System.out.println("seven");
+            case 10 -> System.out.println("ten");
+            case 20 -> System.out.println("twenty");
+            default -> System.out.println("default");
         }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if (year % 100 == 0 && (year / 100) % 10 != 0 && year % 400 != 0 && year % 4 == 0) {
+            System.out.println("not leap");
+        } else System.out.println("leap");
     }
 }
